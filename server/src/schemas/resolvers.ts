cc48bde629a7c250f43c05b82e7d2a913eae0fd3
Timothy Ehli <tehli1882@gmail.com>
2025-03-03 12:56:29 -0500

    progress made in issue 3 and 4

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,6 +1,87 @@
-import bookSchema from '../models/Book';
-import User from '../models/User';
+import { AuthenticationError } from 'apollo-server-express';
+import User from '../models/User.js';
+import { signToken } from '../services/auth.js';
 
-const resolvers = {};
+interface Context {
+  user?: any; // You can replace 'any' with a more specific type if available.
+}
+
+const resolvers = {
+  Query: {
+    me: async (_parent: any, _args: any, context: Context) => {
+      if (!context.user) {
+        throw new AuthenticationError('Not logged in');
+      }
+      const userData = await User.findById(context.user._id).select(
+        '-__v -password'
+      );
+      return userData;
+    },
+  },
+  Mutation: {
+    login: async (
+      _parent: any,
+      { email, password }: { email: string; password: string },
+      _context: Context
+    ) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("Can't find this user");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Wrong password!');
+      }
+      const token = signToken(user.username, user.email, user._id);
+      return { token, user };
+    },
+    addUser: async (
+      _parent: any,
+      {
+        username,
+        email,
+        password,
+      }: { username: string; email: string; password: string },
+      _context: Context
+    ) => {
+      const user = await User.create({ username, email, password });
+      if (!user) {
+        throw new Error('Something went wrong while creating the user');
+      }
+      const token = signToken(user.username, user.email, user._id);
+      return { token, user };
+    },
+    saveBook: async (
+      _parent: any,
+      { book }: { book: any }, // Replace 'any' with a proper type matching your BookInput, if available.
+      context: Context
+    ) => {
+      if (!context.user) {
+        throw new AuthenticationError('You need to be logged in!');
+      }
+      const updatedUser = await User.findByIdAndUpdate(
+        context.user._id,
+        { $addToSet: { savedBooks: book } },
+        { new: true, runValidators: true }
+      );
+      return updatedUser;
+    },
+    removeBook: async (
+      _parent: any,
+      { bookId }: { bookId: string },
+      context: Context
+    ) => {
+      if (!context.user) {
+        throw new AuthenticationError('You need to be logged in!');
+      }
+      const updatedUser = await User.findByIdAndUpdate(
+        context.user._id,
+        { $pull: { savedBooks: { bookId } } },
+        { new: true }
+      );
+      return updatedUser;
+    },
+  },
+};
 
 export default resolvers;
--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -1,3 +1,53 @@
-const typeDefs = ``;
+import { gql } from 'apollo-server-express';
+
+const typeDefs = gql`
+  # Book type used in User.savedBooks
+  type Book {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  # User type
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  # Auth type returned by login and addUser mutations
+  type Auth {
+    token: String!
+    user: User!
+  }
+
+  # Query type
+  type Query {
+    me: User
+  }
+
+  # Input type for saving a book
+  input BookInput {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  # Mutation type for login, user creation, and book management
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: BookInput!): User
+    removeBook(bookId: String!): User
+  }
+`;
 
 export default typeDefs;
